a03fac67ad0377e7e378bc7259201622d3751e3c
lspaddle <lspaddle@gmail.com>
2014-07-06 18:04:55 -0500

    Added all the files

---
+++ src/main/java/com/lspaddle/am/AdvancedMechanics.java
@@ -0,0 +1,31 @@
+package com.lspaddle.am;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "AdvancedMechanics", name = "Advanced Mechanics", version = "1.7.10-1.0")
+public class AdvancedMechanics
+{
+    @Mod.Instance("AdvancedMechanics")
+    public static AdvancedMechanics instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
